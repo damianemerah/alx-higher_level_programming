926146bb0be976e139bc6c6767e9d27293d4d89f
dammy995 <damianemerah1@gmail.com>
2022-12-02 05:42:59 -0800

    magic calcualtor

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    from add_0 import add, sub
+
+    def magic_calculation(a, b):
+        if a < b:
+            c = add(a, b)
+            for i in range(4, 6):
+                c = add(c, i)
+            return c
+        return sub(a, b)
