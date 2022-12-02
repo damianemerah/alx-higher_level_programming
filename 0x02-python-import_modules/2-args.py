6c404518cb0e59c4e999bbdf4f826cb9cd2bff08
dammy995 <damianemerah1@gmail.com>
2022-12-01 17:11:47 -0800

    task 3

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,7 @@ if __name__ == '__main__':
     if len(sys.argv) == 1:
         print('0 arguments.')
     elif len(sys.argv) > 1:
-        print('{} argument{}:'.format(len(sys.argv) - 1, 's' if (len(sys.argv) - 1) > 1 else ''))
+        print('{} argument{}:'.format(len(sys.argv) - 1,
+                                      's' if (len(sys.argv) - 1) > 1 else ''))
         for i in range(len(sys.argv) - 1):
             print("{}: {}".format(i + 1, sys.argv[i + 1]))
