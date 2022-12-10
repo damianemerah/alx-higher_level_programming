66fb662f5ab3708ddbcdf5c6f2c8be280d2f46fb
dammy995 <damianemerah1@gmail.com>
2022-12-10 14:58:50 -0800

    all task

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -4,7 +4,7 @@ def weight_average(my_list=[]):
         denom = 0
         num = 0
         for x in my_list:
-            num += (x[0] + x[1])
+            num += (x[0] * x[1])
             denom += x[1]
         return (num/denom)
     return 0
