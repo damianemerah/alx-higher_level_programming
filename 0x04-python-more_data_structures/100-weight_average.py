1b385ed54698761b17de90cf3e5cf09b3d778e74
dammy995 <damianemerah1@gmail.com>
2022-12-10 14:12:47 -0800

    all task

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if len(my_list) and my_list:
+        denom = 0
+        num = 0
+        for x in my_list:
+            num += x[0] + x[1]
+            denom += x[1]
+        return (num/denom)
+    return 0
