3878e06866966e1ea206946274e93c870e452819
dammy995 <damianemerah1@gmail.com>
2022-12-20 12:30:12 -0800

    task 6,7

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python3
-
 import sys
 
 def safe_print_integer_err(value):
@@ -8,3 +6,4 @@ def safe_print_integer_err(value):
         return True
     except (TypeError, ValueError):
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return False
