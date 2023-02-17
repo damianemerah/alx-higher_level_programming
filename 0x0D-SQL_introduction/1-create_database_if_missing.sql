877c4698937754d72c1cf55ff1bff927e38d3520
dammy995 <damianemerah1@gmail.com>
2023-02-17 05:28:15 -0800

    task 1

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- creates database
+-- does not fail
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+-- Deletes database hbtn_0c_0
+-- doest not fail
+DROP DATABASES IF EXISTS hbtn_0c_0;
