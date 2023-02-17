414a5b8e657f72d905d9afdac0eb2c101cbdcc84
dammy995 <damianemerah1@gmail.com>
2023-02-17 13:09:36 -0800

    all task

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- lists all records of the table except those qith no value
-SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY socre DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
