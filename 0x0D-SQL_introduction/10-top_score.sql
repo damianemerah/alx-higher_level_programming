078841a8024525450f91f40eeaf3ef2daba0cf42
dammy995 <damianemerah1@gmail.com>
2023-02-17 06:17:33 -0800

    task 11

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- list all the records of the table second_table of htbn_0c_0 db
+-- results display score and name
+-- ordered by score
+SELECT score, name FROM second_table ORDER BY score DESC;
---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,4 @@
+-- list all in second_table with score >= 10 in hbtn)=_0c_0 db
+-- result displays score and name
+-- ordered by score (desc)
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
