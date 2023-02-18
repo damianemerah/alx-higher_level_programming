b67d23867c83ac543cc56f012a760f258c65114b
dammy995 <damianemerah1@gmail.com>
2023-02-18 05:27:08 -0800

    task 0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,2 @@
 -- list all privileges of the users user_0d_1 and user_0d_2
-SHOW GRANT FOR 'user_0d_1'@'localhost';
-SHOW GRANT FOR 'user_0d_2'@'localhost';
+SHOW GRANT FOR "user_0d_1@localhost" AND "user_0d_2@localhost";
