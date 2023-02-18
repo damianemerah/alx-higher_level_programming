9c76661d477e301ee372719b09c1c902d5b27124
dammy995 <damianemerah1@gmail.com>
2023-02-18 05:30:16 -0800

    task 0

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,3 +1,4 @@
 -- creates the table force_name(id,name)
 -- should not fail
-CREATE TABLE IF NOT EXISTS 'force_name' (id INT, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS force_name
+(id INT, name VARCHAR(256));
