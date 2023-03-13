69010e99a616ddab53ad3ee2b5e4a630e406c939
Damian <damianemerah1@gmail.com>
2023-03-13 22:39:20 +0000

    all task

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+let x = Math.floor(Number(process.argv[2]));
+if (isNaN(x))
+	console.log('Missing number if occurrences');
+else
+{
+	while (x > 0)
+	{
+		console.log('C is fun');
+		x--;
+	}
+}
