7a81743dfc9b8a5ec4eff7deed9e742366fb6c3f
Damian <damianemerah1@gmail.com>
2023-03-13 17:49:27 +0000

    semistandard

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,4 +2,4 @@
 
 const count = process.argv.length;
 
-console.log(count === 3 ? 'Argument found': count > 3 ? 'Arguments found' : 'No argument found');
+console.log(count === 3 ? 'Argument found' : count > 3 ? 'Arguments found' : 'No argument found');
