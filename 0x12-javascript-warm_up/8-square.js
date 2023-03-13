d764bd14c0f064e245565fabb3d3e5aef86d44eb
Damian <damianemerah1@gmail.com>
2023-03-13 22:41:37 +0000

    all task

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,17 +2,12 @@
 
 const x = Math.floor(+process.argv[2]);
 
-if (isNaN(x))
-	console.log('Missing size');
-else
-{
-	let y = x;
-	while (y > 0)
-	{
-		let row = '';
-		for(let i = 0; i < x; i++)
-			row += 'X';
-		console.log(row);
-		y--;
-	}
+if (isNaN(x)) { console.log('Missing size'); } else {
+  let y = x;
+  while (y > 0) {
+    let row = '';
+    for (let i = 0; i < x; i++) { row += 'X'; }
+    console.log(row);
+    y--;
+  }
 }
