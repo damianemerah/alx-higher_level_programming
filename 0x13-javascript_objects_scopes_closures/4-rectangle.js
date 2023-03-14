d95b87f1a9f664bb9e76b5f360fc9695fe1155a7
Damian <damianemerah1@gmail.com>
2023-03-14 22:26:42 +0000

    all task

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) { [this.width, this.heigth] = [w, h]; }
+  }
+
+  print () {
+    for (let i = 0; i < this.heigth; i++) { console.log('X'.repeat(this.width)); }
+  }
+
+  double () {
+    this.width *= 2;
+    this.heigth *= 2;
+  }
+};
