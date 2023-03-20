5d088306b4c59f8c217e2bc6520e995095954486
dammy995 <damianemerah1@gmail.com>
2023-03-19 18:04:44 -0700

    tak 5

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+This script takes in an argument and
+displays all values in the states
+where `name` matches the argument
+from the database `hbtn_0e_0_usa`.
+This time the script is safe from
+MySQL injections!
+"""
 
 import MySQLdb
 from sys import argv
@@ -6,11 +14,11 @@ from sys import argv
 if __name__ == '__main__':
     """
     Access to the database and get the states
-    from database
+    from the database.
     """
 
-    db = MySQLdb.connect(host='localhost', user=argv[1], port=3306,
-            passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
 
     with db.cursor() as cur:
         cur.execute("""
@@ -22,11 +30,12 @@ if __name__ == '__main__':
                 name LIKE BINARY %(name)s
             ORDER BY
                 states.id ASC
-        """, {'name': argv[4]}
-        )
+        """, {
+            'name': argv[4]
+        })
+
         rows = cur.fetchall()
 
-        if rows is not None:
-            for row in rows:
-                print(row)
-
+    if rows is not None:
+        for row in rows:
+            print(row)
