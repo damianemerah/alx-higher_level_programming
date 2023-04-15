c30ee99c7b8cbaff686a88ce943792699d2676a5
Damian Emerah <damianemerah1@gmail.com>
2023-04-15 13:13:09 -0700

    file update

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
-#/bin/bash
+#!/bin/bash
 # Send a GET request to the URL and display the body of the response
 curl -sL "$1"
--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+#send a request to a url and display only the status code of the response
 curl -s -o /dev/null -w "%{http_code}" "$1"
