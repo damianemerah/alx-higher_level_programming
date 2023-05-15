060adee8e6c75223c293fbbd4d61b78df74ccabb
Damian <damianemerah1@gmail.com>
2023-05-15 00:02:46 +0000

    done

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,27 +1,27 @@
 #!/usr/bin/node
 
 const request = require('request');
-const id = process.argv[2];
-const url = 'https://swapi-api.alx-tools.com/api/films/' + id; 
 
-request.get(url, (error, response, body) => {
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.altx-extensions.com/api/films/${movieId}/`;
+
+request.get(apiUrl, (error, response, body) => {
   if (error) {
-    console.log(error);
+    console.error(error);
     return;
-  } else {
-    const content = JSON.parse(body);
-    const characters = content.characters;
-    // console.log(characters);
-    for (const character of characters) {
-      request.get(character, (error, response, body) => {
-        if (error) {
-          console.log(error);
-	  return;
-        } else {
-          const names = JSON.parse(body);
-          console.log(names.name);
-        }
-      });
-    }
   }
+
+  const movieData = JSON.parse(body);
+
+  movieData.characters.forEach((characterUrl) => {
+    request.get(characterUrl, (error, response, body) => {
+      if (error) {
+        console.error(error);
+        return;
+      }
+
+      const characterData = JSON.parse(body);
+      console.log(characterData.name);
+    });
+  });
 });
